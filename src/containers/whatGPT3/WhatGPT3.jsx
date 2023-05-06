10bf8e3a042f570ca04d4992aafe5d86f10403e6
Ayanda Mathiso <mathisoayanda@gmail.com>
2023-05-06 23:33:04 +0200

    created the Brand and WhatGPT3 components

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './whatGPT3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
     return (
-        <div>WhatGPT3</div>
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature title='What is GPT-3' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia odio excepturi, sunt vel possimus hic illum qui quibusdam maxime rerum consequatur accusantium dolores quasi provident, ipsa tempore culpa. Quo, odit.' />
+            </div>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className='gpt3__whatgpt3-container'>
+                <Feature title='Chatbots' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia odio excepturi, sunt vel possimus hic illum qui quibusdam maxime rerum consequatur accusantium' />
+                <Feature title='Knowledgebase' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia odio excepturi, sunt vel possimus hic illum qui quibusdam maxime rerum consequatur accusantium' />
+                <Feature title='Education' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia odio excepturi, sunt vel possimus hic illum qui quibusdam maxime rerum consequatur accusantium' />
+            </div>
+        </div>
     )
 }
 
