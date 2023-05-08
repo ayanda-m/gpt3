6a81a6491b70d12af788271462a993b53bc826a7
Ayanda Mathiso <mathisoayanda@gmail.com>
2023-05-08 11:45:26 +0200

    created Features and Possibility components

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './features.css'
 
+const featuresData = [
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, optio! Nobis, quos fugit? Corrupti similique ut quas enim minima blanditiis assumenda nam! Voluptate obcaecati vel hic doloribus nemo magni quaerat.'
+    },
+    {
+        title: 'Become the tended active',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, optio! Nobis, quos fugit? Corrupti similique ut quas enim minima blanditiis assumenda nam! Voluptate obcaecati vel hic doloribus nemo magni quaerat.'
+    },
+    {
+        title: 'Message or am nothing',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, optio! Nobis, quos fugit? Corrupti similique ut quas enim minima blanditiis assumenda nam! Voluptate obcaecati vel hic doloribus nemo magni quaerat.'
+    },
+    {
+        title: 'Really boy law county',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, optio! Nobis, quos fugit? Corrupti similique ut quas enim minima blanditiis assumenda nam! Voluptate obcaecati vel hic doloribus nemo magni quaerat.'
+    }
+]
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='gpt3__features section__padding' id='features'>
+            <div className='gpt3__features-heading'>
+                <h1 className='gradient__text'>
+                    The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
+                </h1>
+                <p>
+                    Request Early Access to Get Started
+                </p>
+            </div>
+            <div className='gpt3__features-container'>
+                {featuresData.map((item, index) => (
+                    <Feature title={item.title} text={item.text + index} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css'
 
 const Possibility = () => {
     return (
-        <div>Possibility</div>
+        <div className='gpt3__possibility section__padding' id='possibility'>
+            <div className='gpt3__possibility-image'>
+                <img src={possibilityImage} alt='possibility' />
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className="gradient__text">
+                    The possibilies are beyond your imagination
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur reiciendis, illo ad veritatis sunt optio, repellat voluptates natus vel quibusdam unde tempore sapiente minus iusto iste quis cumque temporibus nesciunt.
+                </p>
+                <h4>Request Early Access to Get Started </h4>
+            </div>
+        </div>
     )
 }
 
